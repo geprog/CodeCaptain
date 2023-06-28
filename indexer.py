91cd4fcfca96bf5d4fcfc6f3a05f61ad4d9eff93
aaa006bd <aaa006bd@gmail.com>
2023-06-28 11:18:04 +0200

    fix: removed time measurement for code execution

--- indexer.py
+++ indexer.py
@@ -14,10 +14,7 @@ def generate_index(repo_name):
   repo_path = os.path.join('data', repo_name)
 
   generate_project_structure_description(repo_path)
-  start = time.time()
   embeddings = OpenAIEmbeddings(disallowed_special=())
-  end = time.time()
-  print('embedding generation took:', end - start)
   docs = []
   docs.extend(TextLoader(os.path.join(repo_path, 'project-structure.txt'), encoding='utf-8').load_and_split())
   for dirpath, _, filenames in os.walk(os.path.join(repo_path, 'repo')):
@@ -30,12 +27,10 @@ def generate_index(repo_name):
 
   text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
   texts = text_splitter.split_documents(docs)
-  start = time.time()
+  
   db = DeepLake(dataset_path=os.path.join(repo_path, 'vector_store'), embedding_function=embeddings, overwrite=True)  # dataset would be publicly available
   db.add_documents(texts)
-  end = time.time()
   
-  print('deeplake instantiation and adding documents took: ', end - start)
   print("done")
 
 if __name__ == '__main__':
--- question.py
+++ question.py
@@ -10,12 +10,20 @@ from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
 
+questions = [
+       # "What are collectors?", 
+       # "What are the names of the collectors?"
+       #"What does NewCollector do?",
+       #"Why do we need the collectors?",
+       #"How are the collectors used in the project?",
+       "What impact will the code have if I change the function NewCollector?",
+       "What is the installation process for this project?"
+    ]
 
-def conversation(repo_name):
+def conversation(repo_name, question, chat_history=[]):
     embeddings = OpenAIEmbeddings(disallowed_special=())
     repo_path = os.path.join('data', repo_name)
 
-    start = time.time()
     db = DeepLake(
         dataset_path= os.path.join(repo_path,'vector_store'),
         read_only=True,
@@ -24,25 +32,21 @@ def conversation(repo_name):
     retriever = db.as_retriever()
     end = time.time()
     
-    print('retrieval took: ', end - start)
     retriever.search_kwargs["distance_metric"] = "cos"
     retriever.search_kwargs["fetch_k"] = 100
 
     retriever.search_kwargs["maximal_marginal_relevance"] = True
     retriever.search_kwargs["k"] = 10
     
-    start = time.time()
     model = ChatOpenAI(
         model_name="gpt-3.5-turbo"
     )  # switch to gpt-3.5-turbo if you want
     qa = ConversationalRetrievalChain.from_llm(model, retriever=retriever)
     end = time.time()
     
-    print('chain building took: ', end - start)
-    questions = ["What are collectors?", "What are the names of the collectors?"]
+    
     chat_history = []
     
-    start = time.time()
     for question in questions:
         result = qa({"question": question, "chat_history": chat_history})
         chat_history.append((question, result["answer"]))
@@ -50,7 +54,6 @@ def conversation(repo_name):
         print(f"**Answer**: {result['answer']} \n")
     end = time.time()
     
-    print('question answer took: ',end-start)
 
 if __name__ == "__main__":
-    conversation('kiel-live')
+    conversation('kiel-live',questions)
