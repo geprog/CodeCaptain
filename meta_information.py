fd3086f262cde33c836c63913c99ca221421b429
aaa006bd <aaa006bd@gmail.com>
2023-06-28 10:45:26 +0200

    chore: use local vector store and rearrange project structure

--- meta_information.py
+++ meta_information.py
@@ -1,11 +1,8 @@
 import os
 
-def generate_project_structure_description(project_root='', save_path=''):
-    print(project_root)
-    if project_root == '':
-        return
-    project_name = os.path.normpath(project_root).split(os.sep)[-1]
-    print(save_path)
+def generate_project_structure_description(project_path):
+    repo_path = os.path.join(project_path, 'repo')
+    
     initial_template = 'The folder structure of the Project is described below.\n'
 
     no_files = 'contains no files'
@@ -15,13 +12,13 @@ def generate_project_structure_description(project_root='', save_path=''):
     files_existing = 'also contains files called'
     
     body_desc = ''
-    for (path,folder_names, file_names) in os.walk(project_root):
+    for (path,folder_names, file_names) in os.walk(repo_path):
         parent_folder = path.split(os.sep)[-1]
         folders_desc = 'Folder {} {}'.format(parent_folder, ('{} {}'.format(folders_existing, folder_names)) if len(folder_names) != 0 else no_folders)
         files_desc = 'Folder {} {}'.format(parent_folder, ('{} {}'.format(files_existing, file_names)) if len(file_names) != 0 else no_files)
 
         body_desc = '{}\n {} {}'.format(body_desc, folders_desc, files_desc)
     
-    with open(f'{save_path}/{project_name}.txt', 'w+') as desc:
+    with open(f'{project_path}/project-structure.txt', 'w+') as desc:
         desc.write('{} \n {}'.format(initial_template, body_desc))
     
