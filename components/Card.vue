3764f2829d1d685bfe9666a9f3c490cbdce3c2f9
m.das <m.das@geprog.com>
2023-06-29 17:10:29 +0200

    dark bg removed

--- components/Card.vue
+++ components/Card.vue
@@ -2,7 +2,7 @@
   <div class="h-40 text-wrap">
     <a
       :href="href"
-      class="flex justify-center w-60 p-2 h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      class="flex justify-center w-60 p-2 h-full rounded-lg shadow bg-gray-800 border-gray-700"
       :class="{
         'hover:bg-gray-100 dark:hover:bg-gray-700': href,
       }"
