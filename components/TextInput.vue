f841f3cfbdeb734213bfecb6c9bd063121c87503
Anbraten <anton@ju60.de>
2023-06-30 11:34:20 +0200

    pre search for user

--- components/TextInput.vue
+++ components/TextInput.vue
@@ -1,9 +1,9 @@
 <template>
   <input
-    model-value="value"
+    :value="modelValue"
     @input="$emit('update:model-value', $event.target.value)"
     type="text"
-    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+    class="bg-gray-400 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:border-2 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
   />
 </template>
 
