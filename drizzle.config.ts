ba94153b689c25c7383ba9ed6b15098caf0944c1
Abdullah Al Amin <aaa006bd@gmail.com>
2023-08-03 06:17:35 +0000

    chore: added drivers in drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "drizzle-kit";
+
+type Drivers = 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'; // https://orm.drizzle.team/kit-docs/config-reference
  
 export default {
   schema: ["./schemas","./schemas/*"],
   out: "./migrations",
+  driver: 'better-sqlite' satisfies Drivers,
 } satisfies Config;
