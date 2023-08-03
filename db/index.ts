99721ae357b0042718f0294180eca8f724940ab5
Abdullah Al Amin <aaa006bd@gmail.com>
2023-08-03 07:17:21 +0000

    chore: added databaseInstance

---
+++ db/index.ts
@@ -0,0 +1,19 @@
+
+import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
+import Database from 'better-sqlite3';
+
+export default class DatabaseInstance {
+    private static db: BetterSQLite3Database;
+
+    private constructor() {};
+
+    public static getDBInstance(): BetterSQLite3Database{
+       if(!this.db){
+        const sqlite = new Database(process.env.DATABASE_NAME);
+        this.db = drizzle(sqlite);
+       }
+
+       return this.db;
+    }
+}
+
