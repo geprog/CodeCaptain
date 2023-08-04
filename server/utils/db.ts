2983e6cdb704f9a70dadc6bc9bb5ca2f3b2b64b3
Anbraten <anton@ju60.de>
2023-08-04 09:39:54 +0200

    nits

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,5 +1,5 @@
 import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 
-const sqlite = new Database(`${process.env.DATABASE_NAME}.db`);
+const sqlite = new Database(process.env.DATABASE_NAME ?? 'code_captain.db');
 export const db: BetterSQLite3Database = drizzle(sqlite);
