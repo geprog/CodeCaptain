b6587346d313327bddd6c0ba419e5786645d08a4
Abdullah Al Amin <aaa006bd@gmail.com>
2023-08-09 10:02:43 +0000

    chore: types updated

--- server/forges/types.ts
+++ server/forges/types.ts
@@ -1,15 +1,20 @@
 import { H3Event } from 'h3';
 
+export type Tokens = {accessToken: string, refreshToken: string, rtExpires?: number}
+export type Credentials = {
+  username: string;
+  password: string;
+}
+export type UserInfo = {
+  name?: string;
+  avatarUrl?: string;
+  email?: string;
+  remoteUserId: string;
+  tokens: Tokens;
+}
+
 export abstract class Forge {
-  public abstract getCloneCredentials(todo: unknown): Promise<{
-    username: string;
-    password: string;
-  }>;
-  public abstract oauthCallback(event: H3Event): Promise<{
-    name?: string;
-    avatarUrl?: string;
-    email?: string;
-    remoteUserId: string;
-  }>;
+  public abstract getCloneCredentials(todo: unknown): Promise<Credentials>;
+  public abstract oauthCallback(event: H3Event): Promise<UserInfo>;
   public abstract getOauthRedirectUrl(o: { state: string }): string;
 }
