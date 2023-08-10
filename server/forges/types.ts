355c78bf72d04479ed9d62a469150fd5167e89f5
Abdullah Al Amin <aaa006bd@gmail.com>
2023-08-10 08:12:55 +0000

    fix: types for forges

--- server/forges/types.ts
+++ server/forges/types.ts
@@ -10,27 +10,20 @@ export type UserInfo = {
   avatarUrl?: string;
   email?: string;
   remoteUserId: string;
- 
 };
 
 export abstract class Forge {
   public abstract getCloneCredentials(todo: unknown): Promise<Credentials>;
   public abstract getOauthRedirectUrl(o: { state: string }): string;
   public abstract getTokens(event: H3Event): Promise<Tokens>;
-  public abstract getUserInfo(token:string): Promise<any>;
+  public abstract getUserInfo(token:string): Promise<UserInfo>;
 
   public async oauthCallback(event: H3Event, tokens: Tokens): Promise<UserInfo> {
     const validTokens = tokens ? tokens : await this.getTokens(event);
     const token = validTokens.accessToken;
 
+    const userInfo = await this.getUserInfo(token);
 
-    const user = await this.getUserInfo(token);
-
-    return {
-      name: user.name || undefined,
-      avatarUrl: user.avatar_url || undefined,
-      email: user.email || undefined,
-      remoteUserId: user.id.toString(),
-    };
+    return userInfo;
   }
 }
