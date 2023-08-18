288e08ce7c8f1897a82385b92b7a1f7551575252
Abdullah Al Amin <aaa006bd@gmail.com>
2023-08-18 08:16:20 +0000

    chore: moved indexing from cloning

--- server/api/repos/[repo_id]/clone.post.ts
+++ server/api/repos/[repo_id]/clone.post.ts
@@ -120,22 +120,5 @@ export default defineEventHandler(async (event) => {
     break;
   }
 
-  // start indexing
-  console.log('start indexing ...');
-  const indexingResponse = await $fetch(`${config.api.url}/index`, {
-    method: 'POST',
-    body: {
-      repo_name: repoId,
-    },
-  });
-
-  if (indexingResponse.error) {
-    console.error(indexingResponse.error);
-    throw createError({
-      statusCode: 500,
-      statusMessage: 'cannot index repo',
-    });
-  }
-
   return 'ok';
 });
