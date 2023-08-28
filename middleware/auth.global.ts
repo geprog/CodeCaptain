419e44bcc89993b2d30f33f556c6ee0cb5159d64
Anbraten <anton@ju60.de>
2023-08-28 20:50:52 +0200

    chore: undo debug

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,9 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  console.log('before auth');
-
   const { user } = await useAuth();
-
-  console.log('user', user.value, !!user.value, to.path);
 
   if (!user.value && to.path !== '/auth/login') {
     return navigateTo('/auth/login');
