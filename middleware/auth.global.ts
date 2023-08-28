4fa316aaea1a05180ff1ec5daf50bc09c858925d
Anbraten <anton@ju60.de>
2023-08-28 19:43:53 +0200

    fix: add missing auth middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,5 +1,9 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+  console.log('before auth');
+
   const { user } = await useAuth();
+
+  console.log('user', user.value, !!user.value, to.path);
 
   if (!user.value && to.path !== '/auth/login') {
     return navigateTo('/auth/login');
