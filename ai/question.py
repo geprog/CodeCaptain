ad5ab4b06ab1a81272701eba9155c19cfc9a8614
Anbraten <anton@ju60.de>
2023-08-29 11:38:55 +0200

    feat: add chat history

--- ai/question.py
+++ ai/question.py
@@ -4,6 +4,7 @@ import time
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ChatMessageHistory
 from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 
@@ -11,8 +12,12 @@ load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 data_path = os.getenv("DATA_PATH")
 
+chats = {}
+
 
 def ask(repo_id: int, question: str, chat_history=[]):
+    history = ChatMessageHistory()
+
     embeddings = OpenAIEmbeddings(disallowed_special=())
 
     repo_path = os.path.join(data_path, str(repo_id))
@@ -27,6 +32,26 @@ def ask(repo_id: int, question: str, chat_history=[]):
     retriever.search_kwargs["maximal_marginal_relevance"] = True
     retriever.search_kwargs["k"] = 20
 
+    template = """You are a chatbot having a conversation with a human.
+
+    Given the following extracted parts of a long document and a question, create a final answer.
+
+    {context}
+
+    {chat_history}
+    Human: {human_input}
+    Chatbot:"""
+
+    prompt = PromptTemplate(
+        input_variables=["chat_history", "human_input", "context"], template=template
+    )
+    memory = ConversationBufferMemory(
+        memory_key="chat_history", input_key="human_input"
+    )
+    chain = load_qa_chain(
+        OpenAI(temperature=0), chain_type="stuff", memory=memory, prompt=prompt
+    )
+
     model = ChatOpenAI(model_name="gpt-3.5-turbo-16k")
     qa = ConversationalRetrievalChain.from_llm(model, retriever=retriever)
     end = time.time()
@@ -37,6 +62,11 @@ def ask(repo_id: int, question: str, chat_history=[]):
     return result["answer"], chat_history
 
     end = time.time()
+
+
+def closeChat(repo_id: int):
+    pass
+    #  delete chats[repo_id]
 
 
 if __name__ == "__main__":
