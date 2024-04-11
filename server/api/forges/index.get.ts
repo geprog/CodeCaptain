91ee3a455c725011dabff604c8438d4f63140752
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-04-11 14:37:41 +0200

    fix

--- server/api/forges/index.get.ts
+++ server/api/forges/index.get.ts
@@ -4,8 +4,6 @@ import { forgeSchema } from '~/server/schemas';
 export default defineEventHandler(async (event) => {
   const user = await requireUser(event);
 
-  console.log('user', user.id);
-
   const forges = await db.select().from(forgeSchema).where(eq(forgeSchema.owner, user.id)).all();
 
   // use map to hide client id and secret
