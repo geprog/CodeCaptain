e07414ff10bb08187d824237f3a239bfda5caabe
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-06-12 09:14:23 +0200

    rm unused

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,4 +1,4 @@
-import type { H3Event, SessionConfig } from 'h3';
+import type { H3Event } from 'h3';
 import { type User, forgeSchema, repoSchema, userForgesSchema, userReposSchema, userSchema } from '../schemas';
 import { and, eq } from 'drizzle-orm';
 import { getForgeFromDB, ForgeApi } from '~/server/forges';
